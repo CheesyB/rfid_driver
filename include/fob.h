27cb9a391dccbce5f1af28ad968346a37023efc5
Ethan Partidas <ethanpartidas@gmail.com>
2024-03-01 09:42:53 -0500

    modify fob api

--- include/fob.h
+++ include/fob.h
@@ -2,11 +2,14 @@
 
 #include "drivers/rc522.h"
 
+int fob_select(const struct spi_dt_spec *rc522, uint8_t *UID);
+int fob_auth(const struct spi_dt_spec *rc522, const uint8_t *UID, const uint8_t sector, const uint8_t *key);
+
 /* Writes the new key to the designated sector. */
-int fob_write_key(const struct spi_dt_spec *rc522, const uint8_t sector, const uint8_t *old_key, const uint8_t *new_key);
+int fob_write_key(const struct spi_dt_spec *rc522, const uint8_t *UID, const uint8_t sector, const uint8_t *key);
 
 /* Writes the passcode to the designated sector. */
-int fob_write_passcode(const struct spi_dt_spec *rc522, const uint8_t sector, const uint8_t *key, const uint8_t *passcode);
+int fob_write_passcode(const struct spi_dt_spec *rc522, const uint8_t *UID, const uint8_t sector, const uint8_t *passcode);
 
 /* Reads the passcode from the designated sector into the passcode array. */
-int fob_read_passcode(const struct spi_dt_spec *rc522, const uint8_t sector, const uint8_t *key, uint8_t *passcode);
+int fob_read_passcode(const struct spi_dt_spec *rc522, const uint8_t *UID, const uint8_t sector, uint8_t *passcode);
